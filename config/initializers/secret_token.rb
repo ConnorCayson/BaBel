52a8b057f5ac9a48af14adad0253a6e1022f366d
Connor Cayson <cpcayson@gmail.com>
2013-08-15 00:11:06 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BaBel::Application.config.secret_key_base = '9c5cc4baf098d3125aa06f5e17f467b863c937cfe298ff498a2a9e863196103662ffb1990d2557d1a9f02d9e3fad8d35c41210fc326242a9cb702f067987d141'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BaBel::Application.config.session_store :cookie_store, key: '_BaBel_session'
