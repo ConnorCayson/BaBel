3533099ddf9eef5f1524235481e8dc687cc2723c
Connor Cayson <cpcayson@gmail.com>
2013-08-20 20:08:30 -0400

    Add Pins Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   validates_presence_of :name
+
+  has_many :pins, :dependent => :destroy
 end
